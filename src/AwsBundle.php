fc73e58947fed219cab8563d0fd4f4faa6a6ad15
Raphael De Freitas <raphael@de-freitas.net>
2018-03-07 12:51:48 +0100

    Enable the StreamWrapper feature

--- src/AwsBundle.php
+++ src/AwsBundle.php
@@ -7,4 +7,19 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class AwsBundle extends Bundle
 {
     const VERSION = '2.0.0';
+    
+    /**
+     * @inheritDoc
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // Register S3 StreamWrapper
+        if ($this->container->has('aws.s3')) {
+            /** @var S3Client $s3 */
+            $s3 = $this->container->get('aws.s3');
+            $s3->registerStreamWrapper();
+        }
+    }
 }
