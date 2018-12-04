6eec592d99d9b2d0aed470682f43fce0de106897
Olivier Dolbeau <git@a.bbnt.me>
2018-12-05 00:47:19 +0100

    Avoid deprecated notice when using symfony/config > 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,8 +10,12 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder;
-        $treeBuilder->root('aws', 'variable');
+        $treeBuilder = new TreeBuilder('aws', 'variable');
+
+        // Keep compatibility with symfony/config < 4.2
+        if (!method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->root('aws', 'variable');
+        }
 
         return $treeBuilder;
     }
