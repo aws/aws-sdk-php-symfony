713435633c3ec564543d5906f158dcc9b04c8fa9
Matteo Galli <thinkindie@mac.com>
2019-01-06 00:21:19 +0100

    Fix deprecation for symfony/config 4.2+
    Heavily inspired by https://github.com/symfony/maker-bundle/pull/324

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,8 +10,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder;
-        $treeBuilder->root('aws', 'variable');
+        // Most recent versions of TreeBuilder have a constructor
+        if (\method_exists(TreeBuilder::class, '__construct')) {
+            $treeBuilder = new TreeBuilder('aws', 'variable');
+        } else { // which is not the case for older versions
+            $treeBuilder = new TreeBuilder;
+            $treeBuilder->root('aws', 'variable');
+        }
 
         return $treeBuilder;
     }
